cd832081e537994ac4e1f160baa03ca85f962782
Lee Dongwoo <dylan.dw.lee@gmail.com>
2022-05-31 15:50:41 +0900

    chore: scaffold

---
+++ apps/docs/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "gongbiz-design-system";
+
+export default function Docs() {
+  return (
+    <div>
+      <h1>Docs</h1>
+      <Button />
+    </div>
+  );
+}
---
+++ apps/web/next.config.js
@@ -0,0 +1,5 @@
+const withTM = require("next-transpile-modules")(["gongbiz-design-system"]);
+
+module.exports = withTM({
+  reactStrictMode: true,
+});
