c2ee932e763de2baf869c47b587e55619f765dce
Lee Dongwoo <dylan.dw.lee@gmail.com>
2022-05-31 16:19:51 +0900

    chore: styled-components 추가

--- packages/gongbiz-design-system/Button.tsx
+++ packages/gongbiz-design-system/Button.tsx
@@ -1,4 +1,5 @@
-import * as React from "react";
-export const Button = () => {
-  return <button>Boop</button>;
-};
+import styled from 'styled-components'
+
+const Button = styled.button``
+
+export default Button
